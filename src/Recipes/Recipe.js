089c374aea04f3f77db98602488f0f79fabc1c29
Ørjan Markhus Lillevik <olillevik@gmail.com>
2018-03-27 14:28:28 +0200

    Adding name and description to recipe view.

--- src/Recipes/Recipe.js
+++ src/Recipes/Recipe.js
@@ -6,6 +6,8 @@ import withLogging from "../withLogging";
 const Recipe = props =>
     <div className="Recipe">
         <Heading/>
+        <h1>{props.recipe.name}</h1>
+        <div>{props.recipe.description}</div>
     </div>;
 
 export default withLogging(Recipe);
